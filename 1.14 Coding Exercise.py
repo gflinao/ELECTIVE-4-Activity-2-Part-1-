c165c461e2927e589c5cd0fd19d06447fbb215e8
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2022-05-24 21:06:43 +0800

    First Certifcate done!

---
+++ 1.14 Coding Exercise.py
@@ -0,0 +1,8 @@
+print("Roses are red, Violets are blue")
+
+if 10 > 7:
+    print("Ten is greater than seven!")
+if 16 < 42:
+    print("Sixteen is less tha forty-two")
+
+print("A long time ago, in a galaxy far, far away...")
---
+++ 4.8 Coding Exercise.py
@@ -0,0 +1,12 @@
+furniture = ["tablet", "chair", "cabinet", "desk", "couch"]
+for x in furniture:
+    if x == "cabinet":
+        continue
+    print(x)
+
+i = 1
+while i < 15:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 15")
